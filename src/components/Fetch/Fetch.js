cbdcb37fa52c0405d8ae350976499e185a2eff31
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-02-04 14:13:02 +0200

    Edit api-key variable

--- src/components/Fetch/Fetch.js
+++ src/components/Fetch/Fetch.js
@@ -3,7 +3,7 @@ export async function FetchTrending() {
   const API_KEY = '635ac74a11f3f55a21a984cd556f5915';
 
   const response = await axios.get(`
-  https://api.themoviedb.org/3/trending/all/day?api_key=635ac74a11f3f55a21a984cd556f5915`);
+  https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`);
   console.log(response);
   return response.data;
 }
