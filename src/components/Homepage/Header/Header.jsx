639cc7c9b2194ce98ba46c6e194a084a3aebf6bc
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-02-04 13:09:21 +0200

    header is done

---
+++ src/components/Homepage/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css';
+export const Header = () => {
+  return (
+    <header>
+      <nav className="navigation">
+        <p>Home</p>
+        <p>Movies</p>
+      </nav>
+    </header>
+  );
+};
