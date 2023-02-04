fbabe9c0f3f6a6f8071aedc9917b1c08fad97c3a
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-02-05 01:42:26 +0200

    styles finished

--- src/Pages/Movies/MovieDetails/MovieDetails.jsx
+++ src/Pages/Movies/MovieDetails/MovieDetails.jsx
@@ -7,45 +7,43 @@ import './MovieDetails.css';
 export const MovieDetails = () => {
   const { movieId } = useParams();
   const [currentMovie, setCurrentMovie] = useState({});
-
   const getPosterUrl = posterPath => {
     return `https://www.themoviedb.org/t/p/w220_and_h330_face${posterPath}`;
   };
   const poster = getPosterUrl(currentMovie.poster_path);
+
   // const genres = currentMovie.genres.map(genre => genre.name);
 
   useEffect(() => {
-    FetchMovieDetails(movieId).then(data => {
-      setCurrentMovie(data);
-    });
+    FetchMovieDetails(movieId)
+      .then(data => setCurrentMovie(data))
+      .catch(error => console.log(error));
   }, [movieId]);
-  console.log(currentMovie);
+
+  // const getReleaseDate = () => {
+  //   return currentMovie.release_date.slice(0, 4);
+  // };
+  // const releaseDate = getReleaseDate();
 
   return (
     <>
-      {movieId && (
-        <div className="movie-container">
-          <img src={poster} alt="poster" className="poster" />
+      <div className="movie-container">
+        <img src={poster} alt="poster" className="poster" />
 
-          {currentMovie && (
-            <div className="text-container">
-              <p className="film-title ">
-                {currentMovie.title}
-                <span>({currentMovie.release_date.slice(0, 4)})</span>
-              </p>
-              <p>
-                <span className="film-bold"> Popularity -</span>
-                {currentMovie.popularity}
-              </p>
-              <p>
-                <span className="film-bold">Overview -</span>
-                {currentMovie.overview}
-              </p>
-              {/* <p>Genres: {genres}</p> */}
-            </div>
-          )}
-        </div>
-      )}
+        {currentMovie && (
+          <div className="text-container">
+            <p className="film-title ">
+              {currentMovie.title}
+              <span >({currentMovie.release_date})</span>
+            </p>
+            <p className="film-bold">Popularity</p>
+            <span className="popularity">{currentMovie.popularity}</span>
+            <p className="film-bold overview">Overview</p>
+            <p className="overview-desc">{currentMovie.overview}</p>
+            {/* <p>Genres: {genres}</p> */}
+          </div>
+        )}
+      </div>
     </>
   );
 };
