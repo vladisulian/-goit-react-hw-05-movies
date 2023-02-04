0ed8fd9088952202770a5265d9690a71b8f910b3
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-02-04 18:26:55 +0200

    Add react-toast

--- src/Pages/Movies/MovieSearch/MoviesSearchBar.jsx
+++ src/Pages/Movies/MovieSearch/MoviesSearchBar.jsx
@@ -1,6 +1,20 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import { useState } from 'react';
 import { FetchByName } from '../FetchByName/FetchByName';
 import './MovieSearchBar.css';
+
+const toastOptions = {
+  position: 'top-right',
+  autoClose: 2000,
+  hideProgressBar: true,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: 'light',
+};
 export const MoviesSearch = ({ setMovies }) => {
   const [Searchbar, setSearchbar] = useState('');
 
@@ -10,24 +24,43 @@ export const MoviesSearch = ({ setMovies }) => {
     e.preventDefault();
 
     if (Searchbar !== '') {
-      FetchByName(Searchbar).then(data => setMovies(data));
+      FetchByName(Searchbar).then(data =>
+        data > 0
+          ? setMovies(data)
+          : toast.error('No films found with this title.', toastOptions)
+      );
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="movie-name">
-        <input
-          type="text"
-          name="movie-name"
-          placeholder="Search film by name"
-          onChange={onSearchBarChange}
-          autoComplete="off"
-        />
-      </label>
-      <button type="submit" className="submit-button">
-        Search
-      </button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="movie-name">
+          <input
+            type="text"
+            name="movie-name"
+            placeholder="Search film by name"
+            onChange={onSearchBarChange}
+            autoComplete="off"
+          />
+        </label>
+        <button type="submit" className="submit-button">
+          Search
+        </button>
+      </form>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      <ToastContainer />
+    </>
   );
 };
