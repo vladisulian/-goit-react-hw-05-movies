ac3ebde524f825561926ef119ce77f9661d9d307
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-02-04 14:18:30 +0200

    Add state with trading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,9 @@ import { FetchTrending } from './Fetch/Fetch';
 import { Header } from './Homepage/Header/Header';
 import { useEffect, useState } from 'react';
 export const App = () => {
+  const [trending, setTrending] = useState([]);
   useEffect(() => {
-    FetchTrending();
+    FetchTrending().then(data => setTrending(data));
   }, []);
   return (
     <>
--- src/components/Fetch/Fetch.js
+++ src/components/Fetch/Fetch.js
@@ -4,6 +4,6 @@ export async function FetchTrending() {
 
   const response = await axios.get(`
   https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`);
-  console.log(response);
-  return response.data;
+//   console.log(response.data.results);
+  return response.data.results;
 }
