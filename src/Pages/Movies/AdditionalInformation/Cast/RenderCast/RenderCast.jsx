d19878cebb8cf56577ec2fe60e5d77330490ef23
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-02-05 17:12:36 +0200

    Style all of cast

--- src/Pages/Movies/AdditionalInformation/Cast/RenderCast/RenderCast.jsx
+++ src/Pages/Movies/AdditionalInformation/Cast/RenderCast/RenderCast.jsx
@@ -5,8 +5,10 @@ export const RenderCast = ({ cast }) => {
   return cast.map(actor => {
     if (actor.profile_path)
       return (
-        <li key={actor.id}>
-          <img src={getPosterUrl(actor.profile_path)} alt='actorPhoto' />
+        <li key={actor.id} className="actor">
+          <img src={getPosterUrl(actor.profile_path)} alt="actorPhoto" />
+          <p className="actor-name">{actor.name}</p>
+          <p className='actor-character'>{actor.character}</p>
         </li>
       );
   });
