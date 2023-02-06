c0866e503f29e9091c80423b6c0259b91f50d1f3
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-02-07 00:42:47 +0200

    Add search query in URL

--- src/Pages/Movies/MovieSearch/MoviesSearchBar.jsx
+++ src/Pages/Movies/MovieSearch/MoviesSearchBar.jsx
@@ -1,9 +1,10 @@
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import './MovieSearchBar.css';
 import { useState } from 'react';
 import { FetchByName } from '../FetchByName/FetchByName';
-import './MovieSearchBar.css';
+import { useSearchParams } from 'react-router-dom';
 
 const toastOptions = {
   position: 'top-right',
@@ -18,18 +19,25 @@ const toastOptions = {
 export const MoviesSearch = ({ setMovies }) => {
   const [Searchbar, setSearchbar] = useState('');
 
+  const [searchParams, setSearchParams] = useSearchParams();
+  const searchQuery = searchParams.get('search') ?? "";
+
+  const updateQueryString = search => {
+    const nextParams = search !== '' ? { search } : {};
+    setSearchParams(nextParams);
+  };
+
   const onSearchBarChange = e => setSearchbar(e.currentTarget.value);
 
   const handleSubmit = e => {
     e.preventDefault();
 
-    if (Searchbar !== '') {
+    if (Searchbar !== '')
       FetchByName(Searchbar).then(data => {
         data.length > 0
           ? setMovies(data)
           : toast.error('No films found with this title.', toastOptions);
       });
-    }
   };
 
   return (
@@ -40,7 +48,12 @@ export const MoviesSearch = ({ setMovies }) => {
             type="text"
             name="movie-name"
             placeholder="Search film by name"
-            onChange={onSearchBarChange}
+            value={searchQuery}
+            onChange={e => {
+              updateQueryString(e.target.value);
+              setSearchParams({ search: e.target.value });
+              onSearchBarChange(e);
+            }}
             autoComplete="off"
           />
         </label>
@@ -48,6 +61,7 @@ export const MoviesSearch = ({ setMovies }) => {
           Search
         </button>
       </form>
+
       <ToastContainer
         position="top-right"
         autoClose={2000}
