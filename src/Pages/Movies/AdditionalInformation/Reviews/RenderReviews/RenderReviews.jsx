08b3f7ecec52911bebad968d62f5f5de02268c9f
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-02-06 19:12:55 +0200

    Edit styles

--- src/Pages/Movies/AdditionalInformation/Reviews/RenderReviews/RenderReviews.jsx
+++ src/Pages/Movies/AdditionalInformation/Reviews/RenderReviews/RenderReviews.jsx
@@ -9,14 +9,17 @@ export const RenderReviews = ({ reviews }) => {
       if (reviews.length !== 0)
         return (
           <li key={review.id} className="review-author">
-            <p className="review-author-name">
-              {review.author} <span>"{review.author_details.username}"</span>
-            </p>
-            {review.author_details.avatar_path ? (
-              <img src={avatar} alt="avatar" className="review-avatar" />
-            ) : (
-              <div className="review-without-avatar">Avatar</div>
-            )}
+            <div className="review-author-info">
+              <p className="review-author-name">
+                {review.author} <span>"{review.author_details.username}"</span>
+              </p>
+              {review.author_details.avatar_path ? (
+                <img src={avatar} alt="avatar" className="review-avatar" />
+              ) : (
+                <div className="review-without-avatar">Avatar</div>
+              )}
+            </div>
+
             <div className="review-text-container">
               <p className="review-text">{review.content}</p>
             </div>
